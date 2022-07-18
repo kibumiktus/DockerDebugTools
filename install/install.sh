d726498705485bc26d13d756c6e9bec776af7f97
kibumiktus <kibum@mail.ru>
2022-07-18 10:39:38 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,6 @@
-apt-get update
-apt-get -y install vim
-apt-get -y install ranger
-apt-get -y install wget
-apt-get -y install less
-apt-get -y install curl
+apt-get update &&
+apt-get -qqy install vim \
+  ranger \
+  wget \
+  less \
+  curl
